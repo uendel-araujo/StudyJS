be37ffd7507399e99d793836fe62d75049bd962d
Uendel Araujo <dendellaraujo@gmail.com>
2022-01-01 11:47:44 -0300

    Created the function CreateP and the Conditions to verificate Weight and Height

--- ExercicioTabelaIMCmodeloDiferente/assets/js/main.js
+++ ExercicioTabelaIMCmodeloDiferente/assets/js/main.js
@@ -2,4 +2,31 @@ const form = document.querySelector('#form');
 
 form.addEventListener('submit', function(event){
     event.preventDefault();
+    const inputWeight = event.target.querySelector('#weight');
+    const inputHeight = event.target.querySelector('#height');
+
+    const weight = Number(inputWeight.value);
+    const height = Number(inputHeight.value);
+
+    if(!weight) {
+        setResult('Invalid Weight', false);
+        return;
+    }
+
+    if(!height) {
+        setResult('Invalid Height', false);
+        return;
+    }
 });
+
+function createP () {
+    const p = document.createElement('p');
+    return p;
+}
+
+function setResult(message, isValid) {
+    const result = document.querySelector('#result');
+    result.innerHTML = '';
+    const p = createP();
+    
+}
